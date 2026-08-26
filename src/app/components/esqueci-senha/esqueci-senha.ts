e78821af0fe2581f2b4e06c4b0f400d77c1fd692
acvb-Alexsander <acvb.developer@gmail.com>
2026-08-26 17:35:53 -0300

    criando header e dando inicio ao login

---
+++ src/app/components/esqueci-senha/esqueci-senha.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-esqueci-senha',
+  styleUrl: './esqueci-senha.scss',
+  templateUrl: './esqueci-senha.html',
+})
+export class EsqueciSenha {}
